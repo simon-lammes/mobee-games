242778b6eb1064fd4a1176a9afc1011a708bf990
Simon Lammes <lammes@sportmed.eu>
2024-03-10 18:23:00 +0100

    introduce mastermind page

---
+++ src/app/mastermind/mastermind.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mastermind',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      mastermind works!
+    </p>
+  `,
+  styles: ``
+})
+export class MastermindComponent {
+
+}
